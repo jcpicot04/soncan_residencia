fe6a62e1df921beb6f2aaf1fe1447871fafa956b
jcpicot04 <jcpicot1718@gmail.com>
2022-12-15 15:14:40 +0100

    responsive for laptop

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -13,7 +13,7 @@ const Services = () => {
        <div className='service'>
         <img className='service-img' src={service1}/>
         <Fade left>
-        <div className='service-text'>
+        <div className='service-text left'>
         <h2>Residencia canina y felina</h2>
         <p className='service-description'>Contamos con 36 boxes con bebedero automático, completamente cubiertos y protegidos del sol, (18 boxes de 2×3 m., 18 boxes de 1,50×3 m., 8 gateras cubiertas), amplios parques de recreo y pistas.</p>
         </div>
@@ -21,7 +21,7 @@ const Services = () => {
        </div>
        <div className='service'>
        <Fade right>
-       <div className='service-text'>
+       <div className='service-text right'>
         <h2>Servicio de peluquería</h2>
         <p className='service-description'>Durante su estancia en nuestras instalaciones ofrecemos servicios de peluquería así como lavado,peinado,secado,etc...</p>
         </div>
@@ -31,7 +31,7 @@ const Services = () => {
        <div className='service'>
         <img className='service-img' src={service3}/>
         <Fade left>
-        <div className='service-text'>
+        <div className='service-text left'>
         <h2>Venta de alimentación para animales</h2>
         <p className='service-description'>Ofrecemos una amplia variedad de alimentos de la mejor calidad para caninos y felinos, tanto para su alimentación durante su estancia en nuestras instalaciones como para venta al público.</p>
         </div>
@@ -39,7 +39,7 @@ const Services = () => {
        </div>
        <div className='service'>
        <Fade right>
-        <div className='service-text'>
+        <div className='service-text right'>
             <h2>Consulta veterinaria 24h</h2>
             <p className='service-description'>Contamos con servicio veterinario 24/7 para atender cualquier problema que pueda tener un animal en nuestras instalaciones lo antes posible, con nosotros está seguro.</p>
         </div>
